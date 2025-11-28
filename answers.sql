cb6240566a6a089a36a4b3bcff924d217c3f9867
Ren-ata10 <rhenamaina850@gmail.com>
2025-11-28 13:55:09 +0300

    Create student table and insert/update records

--- answers.sql
+++ answers.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    fullName VARCHAR(100),
+    age INT
+);
+INSERT INTO student (id, fullName, age) VALUES
+(1, 'John Kamau', 18),
+(2, 'Sarah Ibrahim', 19),
+(3, 'Alice Brown', 21);
+UPDATE student
+SET age = 20
+WHERE id = 2;
